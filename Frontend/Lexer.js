e7d46c9ac9c8766bc4fcabea50faabee18afe16f
Mohammed abdelkafi <kawruusir@gmail.com>
2025-01-26 22:18:51 +0100

    Organization

--- Frontend/Lexer.js
+++ Frontend/Lexer.js
@@ -1,1 +1,11 @@
-
+let text = "";
+let tokens = [];
+let pos = -1
+let curr_char = ""
+function advance() {
+    curr_char=tokens[pos]
+    pos+=1
+}
+function tokenize() {
+    //TODO: impliment Tokenization
+}
