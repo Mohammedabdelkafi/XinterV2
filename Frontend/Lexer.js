af909c9722bc6b98fe1241dc272fff4d5efa20d6
Mohammed abdelkafi <kawruusir@gmail.com>
2025-01-27 12:44:05 +0100

    Working on the lexer

--- Frontend/Lexer.js
+++ Frontend/Lexer.js
@@ -1,11 +1,34 @@
 let text = "";
 let tokens = [];
-let pos = -1
-let curr_char = ""
-function advance() {
-    curr_char=tokens[pos]
-    pos+=1
+let pos = -1;
+let curr_char = "";
+let digits = "1234567890";
+function advance() {;
+    curr_char=tokens[pos];
+    pos+=1;
 }
 function tokenize() {
-    //TODO: impliment Tokenization
+    while (curr_char=="") {
+        if (curr_char=="+") {
+            tokens.append("PLUS");
+        }
+        else if (curr_char=="-") {
+            tokens.append("MINUS");
+        }
+        else if (curr_char in digits) {
+            parsenum()
+        }
+    }
+
+}
+function parsenum() {
+    num_str = "";
+    if (curr_char in digits) {
+        num_str+=curr_char;
+        advance();
+    }
+    else {
+        tokens.append(Number(num_str))
+        advance
+    }
 }
