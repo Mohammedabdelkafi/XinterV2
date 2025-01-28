3e955a80c9668f6bcc591f9ac79535dbee63ff13
Mohammed abdelkafi <kawruusir@gmail.com>
2025-01-28 13:13:36 +0100

    Implemented the parser

--- Interpreter.js
+++ Interpreter.js
@@ -81,4 +81,77 @@ class Lexer {
         console.log(this.tokens);
     }
 }
+
+// Parser
+class Parser {
+    constructor(tokens) {
+        this.tokens = tokens;
+        this.idx = -1;
+        this.curr_tok = null;
+        this.advance();
+        this.parse();
+    }
+
+    advance() {
+        this.idx += 1;
+        if (this.idx < this.tokens.length) {
+            this.curr_tok = this.tokens[this.idx];
+        } else {
+            this.curr_tok = null;
+        }
+    }
+
+    parse() {
+        this.expr();
+    }
+
+    expr() {
+        let result = this.term();
+        while (this.curr_tok === "PLUS" || this.curr_tok === "MINUS") {
+            let op = this.curr_tok;
+            this.advance();
+            if (op === "PLUS") {
+                result += this.term();
+            } else if (op === "MINUS") {
+                result -= this.term();
+            }
+        }
+        console.log("Result: ", result);
+    }
+
+    term() {
+        let result = this.factor();
+        while (this.curr_tok === "MULTIPLY" || this.curr_tok === "DIVIDE") {
+            let op = this.curr_tok;
+            this.advance();
+            if (op === "MULTIPLY") {
+                result *= this.factor();
+            } else if (op === "DIVIDE") {
+                result /= this.factor();
+            }
+        }
+        return result;
+    }
+
+    factor() {
+        let result;
+        if (typeof this.curr_tok === "number") {
+            result = this.curr_tok;
+            this.advance();
+        } else if (this.curr_tok === "LPAREN") {
+            this.advance();
+            result = this.expr();
+            if (this.curr_tok === "RPAREN") {
+                this.advance();
+            } else {
+                throw new Error("Expected ')'");
+            }
+        } else {
+            throw new Error("Unexpected token: " + this.curr_tok);
+        }
+        return result;
+    }
+}
+
 let lexer = new Lexer(text);
+let parser = new Parser(lexer.tokens);
